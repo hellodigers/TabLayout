8054817b906cc17d31171d6c1a9453f510905935
hellodigers <bartosz.kozajda@dige.pl>
2017-01-24 12:51:56 +0100

    Initial commit

---
+++ Example/Pods/Target Support Files/TabLayout/TabLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TabLayout : NSObject
+@end
+@implementation PodsDummy_TabLayout
+@end
---
+++ Example/Pods/Target Support Files/TabLayout/TabLayout-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double TabLayoutVersionNumber;
+FOUNDATION_EXPORT const unsigned char TabLayoutVersionString[];
+
